6d57909af33a0b044cae8d758bc542c2ec9d4179
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2022-10-03 16:29:01 -0700

    a

---
+++ functions.py
@@ -0,0 +1,29 @@
+
+class Student:
+    def __init__(self, lName, fName, gde, croom, bs, gpa, tLname, tFname):
+        self.lName = lName
+        self.fName = fName 
+        self.gde = gde
+        self.croom = croom
+        self.bs = bs
+        self.gpa = gpa
+        self.tlName = tLname
+        self.tfName = tFname
+
+
+
+
+def studentSearch():
+    print("Hello! Please enter any of the following choices (NOTE: Anything in brackets[] is optional input, anything in angle brackets is your input)")
+    print("S[tudent]: <lastname> [B[us]]")
+    userInput = input("Enter your option here: ")
+
+
+    if userInput: int(userInput)
+    print("sick number you chose ", userInput)
+
+    if userInput: input(userInput)
+    print("oh cool, you wrote: ", userInput)
+
+    
+
--- schoolsearch.ext.py
+++ schoolsearch.ext.py
@@ -1,34 +1,19 @@
 import sys
-
-class Student:
-    def __init__(self, lName, fName, gde, croom, bs, gpa, tLname, tFname):
-        self.lName = lName
-        self.fName = fName 
-        self.gde = gde
-        self.croom = croom
-        self.bs = bs
-        self.gpa = gpa
-        self.tlName = tLname
-        self.tfName = tFname
-
-
-def studentSearch(stLastName, stFirstName, grade, classroom, bus, gpa, tLastName, tFirstName):
-    print("check")
-
-
+from functions import *
 
 def main():
     count = 0
+    n = 0
     array =[]
     studentList =[]
     text_file = open("students.txt", "r") 
     array = text_file.read().split(',')
     while count < 60:
-        studentList.append(Student(array[0], array[1], array[2], array[3],array[4], array[5], array[6], array[7]))
+        studentList.append(Student(array[n], array[n+1], array[n+2], array[n+3],array[n+4], array[n+5], array[n+6], array[n+7]))
         count += 1
-
-    for obj in studentList:
-        print(obj.lName)
+        n+=7
+    studentSearch()
+    
 
 if __name__ =="__main__":
     main()
