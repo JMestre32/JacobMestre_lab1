9595ec561302079b998d6055c8e1b1dd4808ee08
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2022-10-03 19:41:20 -0700

    a

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 
+from ast import IsNot
+
+
 class Student:
     def __init__(self, lName, fName, gde, croom, bs, gpa, tLname, tFname):
         self.lName = lName
@@ -7,23 +10,42 @@ class Student:
         self.croom = croom
         self.bs = bs
         self.gpa = gpa
-        self.tlName = tLname
-        self.tfName = tFname
+        self.tLname = tLname
+        self.tFname = tFname
 
 
 
 
-def studentSearch():
+def startMenu(studentList):
     print("Hello! Please enter any of the following choices (NOTE: Anything in brackets[] is optional input, anything in angle brackets is your input)")
     print("S[tudent]: <lastname> [B[us]]")
-    userInput = input("Enter your option here: ")
-
-
-    if userInput: int(userInput)
-    print("sick number you chose ", userInput)
-
-    if userInput: input(userInput)
-    print("oh cool, you wrote: ", userInput)
+    myInput = list(input("Enter your option here: ").split(" "))
+    option = myInput[0]
+    nameNumber = myInput[1]
 
     
+    if (option == "S:" or "Student:") and myInput[2] == "B" or "Bus":
+        studentBusSearch(nameNumber, studentList)
+
+
+def studentSearch(name, studentList):
+    count = 0
+    while count <60:
+        if name != studentList[count].lName:
+            count += 1
+        elif name == studentList[count].lName:
+            print(studentList[count].lName, studentList[count].fName, studentList[count].gde, studentList[count].croom, studentList[count].tLname, studentList[count].tFname)
+            count+=1
+    
+
+def studentBusSearch(name, studentList):
+    count = 60
+    while count <60:
+        if name != studentList[count].lName:
+            count+=1
+        elif name == studentList[count].lName:
+            print(studentList[count].lName, studentList[count].fName, studentList[count].bs)
+            count +=1
+
+
 
--- schoolsearch.ext.py
+++ schoolsearch.ext.py
@@ -7,12 +7,17 @@ def main():
     array =[]
     studentList =[]
     text_file = open("students.txt", "r") 
-    array = text_file.read().split(',')
-    while count < 60:
-        studentList.append(Student(array[n], array[n+1], array[n+2], array[n+3],array[n+4], array[n+5], array[n+6], array[n+7]))
-        count += 1
-        n+=7
-    studentSearch()
+    array = text_file.readlines()
+    #while count < 60:
+     #   studentList.append(Student(textLine[n], textLine[n+1], textLine[n+2], textLine[n+3],textLine[n+4], textLine[n+5], textLine[n+6], textLine[n+7]))
+      #  count += 1
+       # n+=7
+    for line in array:
+       textLine = line.split(",")
+       studentList.append(Student(textLine[0], textLine[1], textLine[2], textLine[3],textLine[4], textLine[5], textLine[6], textLine[7]))
+    print(studentList[0].tFname)
+    print(studentList[1].lName)
+    startMenu(studentList)
     
 
 if __name__ =="__main__":
